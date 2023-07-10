d22e21d02bc6c9aa585862e4eb37f641bf88a3c5
Yun Jia <yunjia@me.com>
2023-07-10 12:54:08 -0700

    chore: Able to move the arm by publishing message.

--- move.sh
+++ move.sh
@@ -1,3 +1,3 @@
 rostopic pub -1 /arm_controller/command \
   trajectory_msgs/JointTrajectory -- \
-  '{joint_names: ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"], points: [{positions: [0.1, -0.5, 0.5, 0.75, 0.1, 0.1], time_from_start: [1.0, 0.0]}]}'
+  '{joint_names: ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"], points: [{positions: [0.1, -0.5, 0.5, 0.75, 0.1, 0.1], time_from_start: [1, 0]}]}'
